f233dab5a2023bce93a07c8e8860cb3b672a4bbb
0GustavoAmorim <gustavoamorilsilva@gmail.com>
2024-05-24 15:16:51 -0300

    Organizando arquivos

---
+++ Filgram.ConsoleApp/Configuration/BotConfiguration.cs
@@ -0,0 +1,7 @@
+﻿namespace Filegram.ConsoleApp.Configuration
+{
+    public class BotConfiguration
+    {
+        public string BotToken { get; set; }
+    }
+}
--- Filgram.ConsoleApp/Program.cs
+++ Filgram.ConsoleApp/Program.cs
@@ -1,72 +1,18 @@
-﻿using Telegram.Bot;
-using Telegram.Bot.Exceptions;
-using Telegram.Bot.Polling;
-using Telegram.Bot.Types;
-using Telegram.Bot.Types.Enums;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Filegram.ConsoleApp.Configuration;
+using Filegram.ConsoleApp.Controllers;
+using Microsoft.Extensions.Configuration;
 
-
-namespace TelegramBotExample
-{
-    class Program
+Host.CreateDefaultBuilder(args)
+    .ConfigureAppConfiguration((context, config) =>
     {
-
-        static async Task Main(string[] args)
-        {
-
-            var botClient = new TelegramBotClient("bot-token");
-
-            using CancellationTokenSource cts = new();
-
-            // StartReceiving não bloqueia a thread que o chamou. O recebimento é feito no ThreadPool.
-            ReceiverOptions receiverOptions = new()
-            {
-                // recebe todos os tipos de atualizações, exceto as atualizações relacionadas a ChatMember
-                AllowedUpdates = Array.Empty<UpdateType>()
-            };
-
-            botClient.StartReceiving(
-                updateHandler: ProcessarAtualizacaoAsync,
-                pollingErrorHandler: ProcessarErroPollingAsync,
-                receiverOptions: receiverOptions,
-                cancellationToken: cts.Token
-            );
-
-            var me = await botClient.GetMeAsync();
-
-            Console.WriteLine($"Começar a ouvir @{me.Username}");
-            Console.ReadLine();
-
-            cts.Cancel();
-
-            async Task ProcessarAtualizacaoAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
-            {
-                //Processar apenas atualizações de mensagens
-                if (update.Message is not { } message)
-                    return;
-                //Processa apenas mensagens de text
-                if (message.Text is not { } messageText)
-                    return;
-
-                var chatId = message.Chat.Id;
-
-                Message sentMessage = await botClient.SendTextMessageAsync(
-                        chatId: chatId,
-                        text: "Você disse:\n" + messageText,
-                        cancellationToken: cancellationToken);
-            }
-
-            Task ProcessarErroPollingAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
-            {
-                var MensagemErro = exception switch
-                {
-                    ApiRequestException apiRequestException
-                        => $"Telegram API Error:\n[{apiRequestException.ErrorCode}]\n{apiRequestException.Message}",
-                    _ => exception.ToString()
-                };
-
-                Console.WriteLine(MensagemErro);
-                return Task.CompletedTask;
-            }
-        }
-    }
-}
+        config.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+    })
+    .ConfigureServices((context, services) =>
+    {
+        services.AddBotConfiguration(context.Configuration);
+        services.AddHostedService<BotController>();
+    })
+    .Build()
+    .Run();
